83937fe78e3da2b3e5da657bd80ab18a83302c36
Montazir Nasser <115963375+montazir0@users.noreply.github.com>
2024-12-06 15:14:50 +0300

    Add files via upload

---
+++ File.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+#include "clsLoginRegisterScreen.h"
+#include "clsString.h"
+using namespace std;
+int main()
+{
+
+    while (clsLoginScreen::ShowLoginScreen());
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
